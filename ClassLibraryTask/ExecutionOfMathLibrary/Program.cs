ff8046269fa7141d6927fa4bd77dddff21e307d9
insidecode-dev <trqtmhdv@gmail.com>
2022-12-30 17:47:08 +0400

    math library

---
+++ ClassLibraryTask/ExecutionOfMathLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MathLibrary;
+namespace ExecutionOfMathLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double val1, val2;
+            Double.TryParse(Console.ReadLine(), out val1);
+            Double.TryParse(Console.ReadLine(), out val2);
+
+            Console.WriteLine("add : {0}", MathClass.Add(ref val1, ref val2));
+            Console.WriteLine("subtract : {0}", MathClass.Subtract(ref val1, ref val2));
+            Console.WriteLine("multiply : {0}", MathClass.Multiply(ref val1, ref val2));
+            Console.WriteLine("divide : {0}", MathClass.Divide(ref val1, ref val2));
+        }
+    }
+}
---
+++ ClassLibraryTask/MathLibrary/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class MathClass
+    {
+        public static double Add(ref double a, ref double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(ref double a, ref double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(ref double a, ref double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(ref double a, ref double b)
+        {
+            return a / b;
+        }
+    }
+}
