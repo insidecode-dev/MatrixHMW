fb88a6c176c5e95593c33de11913a47bcd8f691e
insidecode-dev <trqtmhdv@gmail.com>
2023-01-09 00:05:40 +0400

    bank account project solution

---
+++ BankAccountProject/Helper/CheckingAcct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountProject.Helper
+{
+    public class CheckingAcct:ParentOfAcct
+    {
+        
+        public CheckingAcct(string name, string surname, decimal initBalance)
+        {
+            AccountOwner = name + " " + surname;
+            Balance= initBalance;
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (Balance>=amount)
+            {                
+                Balance -= (amount);
+            }
+            else if (Balance < amount)
+            {
+                Balance -= amount + 35.0m;
+                
+            }
+        }
+
+    }
+}
