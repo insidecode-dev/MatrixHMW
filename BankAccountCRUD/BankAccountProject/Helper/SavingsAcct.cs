4d0972738e26409323189c5240e896eb07e0fd7c
insidecode-dev <trqtmhdv@gmail.com>
2023-02-06 04:55:58 +0400

    bank account project with crud mainly on sql

---
+++ BankAccountCRUD/BankAccountProject/Helper/SavingsAcct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountProject.Helper
+{
+    public class SavingsAcct:ParentOfAcct
+    {
+        private readonly decimal commission=0;
+        private decimal commissionCount=0;
+        public SavingsAcct(string name, string surname, decimal _commission, decimal initBalance)
+        {
+            AccountOwner= name+" "+surname;
+            commission = _commission;
+            Balance= initBalance;
+        }
+        public void ApplyInterest()
+        {            
+            Balance= Balance + Balance * commission;            
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (Balance>=amount)
+            {
+                if (commissionCount >= 3)
+                {
+                    Balance -= amount + 2.0m;
+                    return;
+                }
+                Balance -= amount;
+                commissionCount++;
+            }
+            else if (Balance < amount)
+            {
+                Console.WriteLine("there is not enough money for operating this transaction");
+                return;
+            }
+        }
+    }
+}
