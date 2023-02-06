7bdda16cbf714a230bac5c3e44d428921b457a71
insidecode-dev <trqtmhdv@gmail.com>
2023-02-06 05:19:04 +0400

    solution

---
+++ BasicCRUDApplication/ConnectionHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCRUDApplication
+{
+    public class ConnectionHelper
+    {
+        private string connectionString = "Data Source=DESKTOP-TOURGOO\\SQLEXPRESS;Initial Catalog=BasicBankSystem;Integrated Security=True";
+
+        private SqlConnection con;
+        
+        private SqlDataReader dtRd;
+
+        public SqlConnection Con { get=>con; set=> con = value; }
+        
+        public SqlDataReader DtRd { get => dtRd;set => dtRd = value; }
+
+        public ConnectionHelper()
+        {
+            this.con= new SqlConnection(connectionString);            
+        }
+
+        public bool Connect()
+        {
+            if (this.con.State == System.Data.ConnectionState.Closed || this.con.State == System.Data.ConnectionState.Broken)
+            {
+                try { this.con.Open(); }
+                catch {}
+            }
+            return true;
+        }
+
+        public void DisConnect()
+        {
+            if (this.con.State == System.Data.ConnectionState.Open)
+            {
+                this.con.Close();
+                this.con.Dispose();
+            }
+        } 
+        
+    }
+}
---
+++ BasicCRUDApplication/Person.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCRUDApplication
+{
+    public class Person
+    {
+        private string name = null;
+        private string surName = null;
+        private string sourceBalance = null;
+
+        // encapsulation
+        public string Name { get => name; set => name = value; }
+        public string SurName { get => surName; set => surName = value; }
+        public string SourceBalance { get => sourceBalance; set => sourceBalance = value; }
+
+        public Person(string _name, string _surName, string _sourceBalance)
+        {
+            this.Name = _name;
+            this.SurName = _surName;
+            this.SourceBalance = _sourceBalance;
+        }
+
+        public bool AddPerson(ConnectionHelper connectionHelper)
+        {
+            return this.ExecuteProc(connectionHelper, "add_Person");
+        }
+
+        int ID;
+        public bool UpdatePerson(ConnectionHelper connectionHelper, int _ID)
+        {
+            ID = _ID;
+            return this.ExecuteProc(connectionHelper, "update_Person");
+        }
+
+        public bool DeletePerson(ConnectionHelper connectionHelper, int _ID)
+        {
+            ID = _ID;
+            return this.ExecuteProc(connectionHelper, "delete_Person");
+        }
+
+        public bool ExecuteProc(ConnectionHelper connectionHelper, string prcdName)
+        {
+            try
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.CommandText = prcdName;
+                    command.Connection = connectionHelper.Con;
+
+                    if (prcdName == "add_Person")
+                    {
+                        SqlParameter[] parameters = new SqlParameter[3] {
+                            new SqlParameter("@_name", this.Name),  // input variable must be same as in input variable in stored procedure in sql
+                        new SqlParameter("@_surname", this.SurName),
+                        new SqlParameter("@_SourceBalance", this.SourceBalance)
+                        };
+
+                        foreach (SqlParameter p in parameters)
+                        {
+                            p.Direction = ParameterDirection.Input;
+                            command.Parameters.Add(p);
+                        }
+                        command.ExecuteNonQuery();
+                    }
+
+                    else if (prcdName == "update_Person")
+                    {
+                        SqlParameter[] parameters = new SqlParameter[4] {
+                            new SqlParameter("@_personId", this.ID),
+                            new SqlParameter("@_newName", this.Name),  // input variable must be same as in input variable in stored procedure in sql
+                        new SqlParameter("@_newSurname", this.SurName),
+                        new SqlParameter("@_newSourceBalance", this.SourceBalance)
+                        };
+
+                        foreach (SqlParameter p in parameters)
+                        {
+                            p.Direction = ParameterDirection.Input;
+                            command.Parameters.Add(p);
+                        }
+                        command.ExecuteNonQuery();
+                    }
+
+                    else if (prcdName == "delete_Person")
+                    {
+                        SqlParameter param = new SqlParameter("@_ID", this.ID);
+                        param.Direction = ParameterDirection.Input;
+                        command.Parameters.Add(param);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
